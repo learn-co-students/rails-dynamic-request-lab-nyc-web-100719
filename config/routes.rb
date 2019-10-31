54b66de28d68434b71508e585a13ac518248c60e
Bailey Wood <woodbmae@gmail.com>
2019-10-31 12:34:58 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :students, only: :index
+
+  get '/students', to: 'students#index'
+  get '/students/:id', to: 'students#show'
+  
 end
+
+
+
+# get '/villains', to: 'villains#index', as: 'villains'
+# get '/villains/new', to: 'villains#new', as: 'new_villain'
+# post '/villains', to: 'villains#create'
